d787bc2c8c518b28fdafe6fdc910a00fe3afaef7
Kirillov Alexander <kirillovbrest@gmail.com>
2021-03-30 18:17:09 +0300

    Classes Server and ServerRunnable added

---
+++ src/main/java/ru/netology/server/Server.java
@@ -0,0 +1,37 @@
+package ru.netology.server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+    private final int port;
+    final ExecutorService threadPool = Executors.newFixedThreadPool(64);
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public void start() {
+        final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+
+        try (final ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                try {
+                    final Socket socket = serverSocket.accept();
+                    Runnable serverRunnable = new ServerRunnable(socket, validPaths);
+                    threadPool.submit(serverRunnable);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
---
+++ src/main/java/ru/netology/server/ServerRunnable.java
@@ -0,0 +1,89 @@
+package ru.netology.server;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ServerRunnable implements Runnable {
+
+    private final Socket socket;
+    private final List<String> validPaths;
+
+    public ServerRunnable(Socket socket, List<String> validPaths) {
+        this.socket = socket;
+        this.validPaths = validPaths;
+    }
+
+    @Override
+    public void run() {
+        try (final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());) {
+            // read only request line for simplicity
+            // must be in form GET /path HTTP/1.1
+            final String requestLine = in.readLine();
+            final String[] parts = requestLine.split(" ");
+
+            if (parts.length != 3) {
+                // just close socket
+                socket.close();
+                return;
+            }
+
+            final String path = parts[1];
+            if (!validPaths.contains(path)) {
+                out.write((
+                        "HTTP/1.1 404 Not Found\r\n" +
+                                "Content-Length: 0\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.flush();
+                socket.close();
+                return;
+            }
+
+            final Path filePath = Path.of(".", "public", path);
+            final String mimeType = Files.probeContentType(filePath);
+
+            // special case for classic
+            if (path.equals("/classic.html")) {
+                final String template = Files.readString(filePath);
+                final byte[] content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
+                socket.close();
+                return;
+            }
+
+            final long length = Files.size(filePath);
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            Files.copy(filePath, out);
+            out.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
